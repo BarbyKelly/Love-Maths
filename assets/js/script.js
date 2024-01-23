aa28d609210168f83800e9a38faed0dfef5ec2a9
Barby Kelly <barbykelly1@gmail.com>
2024-01-23 10:24:46 +0000

    Logo sign make it visible, again. Still to fix: Division answer not a whole number, favicon not showing on the tab.

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ document.getElementById("answer-box").addEventListener("keydown", function(event
     if (event.key === "Enter") {
         checkAnswer();
     }
-})
+});
 
     runGame("addition");
 
@@ -137,7 +137,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    operand1 = operand1 * operand2;
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
 }
